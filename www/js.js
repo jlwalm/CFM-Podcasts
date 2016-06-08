87150246da13444f02a01af2d6b88f69f422a576
jlwalm <jeremy.walmsley@gmail.com>
2016-06-08 19:03:25 +0900

    update list function to pass url to gotoplayer function

--- www/js.js
+++ www/js.js
@@ -1,22 +1,94 @@
-// This is a JavaScript file
 
-$.get('http://www.cfmc.org.uk/podcast.xml', function (data) {
-    $(data).find("item").each(function () { // or "item" or whatever suits your feed
+//function to load rss feed.
+
+    var GoToPlayer = function(url, title){
+        
+        alert(url);
+        
+        console.log(url);
+        
+        };
+
+
+
+
+    var Load = function() { 
+        
+        //show message
+        
+        //get podcasts
+        $.ajax({
+   url: 'http://www.cfmc.org.uk/podcast.xml',
+        dataType: 'xml',
+      crossDomain: true,
+   success: function(data){
+            $(data).find("item").each(function () { // or "item" or whatever suits your feed
         var el = $(this);
 
-        console.log("------------------------");
-        console.log("title      : " + el.find("title").text());
-        console.log("Date     : " + el.find("pubdate").text());
-        console.log("description: " + el.find("length").text());
+
         
         
         var title = el.find("title").text();
+        var link = encodeURIComponent(el.find('enclosure').attr('url'));
         
-         var li = $("<li><a href='#'>"+title+"</a></li>");
+        
+         var li = "<li onclick=\"GoToPlayer('"+link+"')\">"+title+"<\/li>";
 
-       $("#feed ul").append(li);
+       $("#feed").append(li);
         
         
         
     });
+
+   
+   
+   },
+   error : function() {
+        $('#status').text('Failed to load podcast feed. Please check your internet connection and try again.');
+      },
+      complete: function(){
+          
+               $('#status').hide();
+    $("#feed").listview('refresh');
+          
+      },
+   timeout: 1000 //in milliseconds
+});};
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+$(document).ready(function(){
+
+    
+
+    Load();
+    GoToPlayer("myurl");
+   
+   
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
